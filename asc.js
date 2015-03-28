8571f0ff864a9bbd8c8973efe4fcae6473e49a92
hao1118 <hao1118@gmail.com>
2015-03-28 13:24:27 +0800

    Create asc.js

---
+++ asc.js
@@ -0,0 +1,22 @@
+// export asc function to global
+
+exports.asc = asc = function (gen) {
+    var fn = gen();
+    function handle(result) {
+        if (result.done)
+            return result.value;
+        if (result.value && typeof result.value.then == "function") {   // Promise
+            return result.value.then(
+                function (res) {
+                    return handle(fn.next(res));
+                },
+                function (err) {
+                    return handle(fn.throw(err));
+                }
+            );
+        }else{
+            return handle(fn.next());  // normal generator
+        }
+    }
+    return handle(fn.next());
+}
