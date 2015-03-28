26420b6785343f941c922fb8acfe711ffe5f1623
hao1118 <hao1118@gmail.com>
2015-03-28 17:56:15 +0800

    Update route.js

--- route.js
+++ route.js
@@ -1,13 +1,13 @@
-// asc function sample to check input and update the model
+//asc function sample to check input and update the model
 
-var DataModel = require('./datamodel')    // DataModel is a Mongoose Document Model
+var DataModel = require('./datamodel')    //DataModel is a Mongoose document model
 
 var check = function(model, url, errors){
     return function*(){     //a generator function
-        var m = yield DataModel.findOne({url: model.url}).exec();    // Mongoose query.exec returns Promise
+        var m = yield DataModel.findOne({url: model.url}).exec();    //Mongoose query.exec returns Promise
         if (m && m.id != model.id){
             errors.url='url already exists.'    //url must be unique
-            return false;
+            return false;  //or throw new Error('url already exists.');
         }
         model.url = url;
         return true;
@@ -16,10 +16,17 @@ var check = function(model, url, errors){
 
 router.post('/savedata/:someid', function (req, res, next) {
     asc(function*() {
-        var model = yield DataModel.findOne({id: someid}).exec();   // Mongoose query.exec returns Promise
+        var model = yield DataModel.findOne({id: someid}).exec();   //Mongoose query.exec returns Promise
         var url = req.body.url;    //try to update the model.url to this url.
         var errors = {};
         var ok = yield asc(check(model, url, errors));    //child asc function inside main asc function, returns generator
+        /* or:
+        try{
+            var ok = yield asc(check(model, url, errors));
+        }catch(err){
+            res.send('ERROR: ' + err.message);
+        }
+        */
         if (ok) {
             yield model.save();
             res.redirect('/home');
@@ -28,3 +35,8 @@ router.post('/savedata/:someid', function (req, res, next) {
         }
     });
 });
+
+//or in this way:
+router.post('/savedata/:someid', asc(function* (req, res, next) {
+    ...
+}));
